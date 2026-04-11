5ab1f3703060ead102326081fe3b4177f43bc56d
wildfield <wildfield@users.noreply.github.com>
2026-04-10 19:00:38 -0700

    added stdin

--- src/main.zig
+++ src/main.zig
@@ -22,26 +22,39 @@ const MessageType = enum {
     KeysLike,
     Delete,
     DeleteIfExists,
+    Stdin,
 };
 
-const KeyValuePair = struct {
-    key: [:0]const u8,
-    value: [:0]const u8,
-};
+// Accepts either regular or sentinel-terminated slice
+fn KeyValuePair(comptime slice: type) type {
+    return struct { 
+        key: slice,
+        value: slice,
+    };
+}
 
-const Message = union(MessageType) {
-    Get: [:0]const u8,
-    GetOrElse: KeyValuePair,
-    Set: KeyValuePair,
-    Keys: void,
-    KeyValues: void,
-    KeysLike: [:0]const u8,
-    Delete: [:0]const u8,
-    DeleteIfExists: [:0]const u8,
-};
+// const KeyValuePair = struct {
+//     key: [:0]const u8,
+//     value: [:0]const u8,
+// };
+
+fn Message(comptime slice: type) type {
+    return union(MessageType) {
+        Get: slice,
+        GetOrElse: KeyValuePair(slice),
+        Set: KeyValuePair(slice),
+        Keys: void,
+        KeyValues: void,
+        KeysLike: slice,
+        Delete: slice,
+        DeleteIfExists: slice,
+        Stdin: void,
+    };
+}
 
 const StateMachineError = error {
     InvalidState,
+    InvalidCommand,
 };
 
 const StateType = enum {
@@ -101,12 +114,12 @@ fn bind_text(
 
 const StateMachine = struct {
     current_state: State = .Initial,
+    stdout: *std.Io.Writer,
     db: *c.sqlite3 = undefined,
-    stdout: *std.Io.Writer = undefined,
     stdout_writer: *std.fs.File.Writer = undefined,
     statement: *c.sqlite3_stmt = undefined,
 
-    fn open(self: *StateMachine, filepath: [:0]const u8, stdout: *std.Io.Writer) !void {
+    fn open(self: *StateMachine, filepath: [:0]const u8) !void {
         if (self.current_state != .Initial) {
             return StateMachineError.InvalidState;
         }
@@ -142,11 +155,10 @@ const StateMachine = struct {
         self.db = db;
         errdefer _ = c.sqlite3_close(self.db);
 
-        self.stdout = stdout;
         self.current_state = .{ .DatabaseOpen = undefined };
     }
 
-    fn process(self: *StateMachine, message: Message) !void {
+    fn process(self: *StateMachine, comptime slice: type, message: Message(slice)) !void {
         switch (self.current_state) {
             .Initial => return StateMachineError.InvalidState, 
             .Closed => return StateMachineError.InvalidState,
@@ -230,6 +242,7 @@ const StateMachine = struct {
                             return DbError.FailedToExecuteQuery;
                         }
                     },
+                    .Stdin => return StateMachineError.InvalidCommand,
                 }
 
                 self.current_state =  .{ .Processing = message };
@@ -405,6 +418,7 @@ const StateMachine = struct {
                     return DbError.FailedToExecuteQuery;
                 }
             },
+            .Stdin => return StateMachineError.InvalidCommand,
         }
     }
 
@@ -433,6 +447,9 @@ const StateMachine = struct {
                     .Get, .GetOrElse, .Keys, .KeyValues, .KeysLike => {
                         _ = c.sqlite3_finalize(self.statement);
                     },
+                    .Stdin => {
+                        std.log.err("Stdin in close operation", .{});
+                    },
                 }
             },
         }
@@ -444,11 +461,46 @@ const StateMachine = struct {
     }
 };
 
+const ArgIteratorWrapper = struct {
+    iterator: *std.process.ArgIterator,
+
+    fn next(self: *const ArgIteratorWrapper) !?[:0]const u8 {
+        return self.iterator.next();
+    }
+};
+
+const DelimiterIterator = struct {
+    reader: *std.Io.Reader,
+    delimiter: u8,
+    is_done: bool = false,
+
+    fn next(self: *DelimiterIterator) !?[]const u8 {
+        if (self.is_done) return null;
+        const result = self.reader.takeDelimiterInclusive(self.delimiter) catch |err| {
+            switch(err) {
+                std.Io.Reader.DelimiterError.EndOfStream => {
+                    self.is_done = true;
+                    return null;
+                },
+                else => {
+                    return err;
+                }
+            }
+        };
+        if (result.len == 0) {
+            self.is_done = true;
+            return null;
+        } else {
+            return result[0..result.len - 1];
+        }
+    }
+};
+
+const usage = "Usage: sqey db_filepath command command_args*";
+
 pub fn main() !u8 {
     var args = std.process.args();
     _ = args.skip();
-
-    const usage = "Usage: sqey db_filepath command command_args*";
 
     const filepath = args.next() orelse {
         std.log.err(usage, .{});
@@ -459,14 +511,39 @@ pub fn main() !u8 {
         const help =
             \\
             \\Usage: sqey <path to the sqlite database> <command> <one or more command arguments>
-            \\Available commands: get, get-or-else, set, keys, key-values, keys-like, delete, delete-if-exists
+            \\Available commands: get, get-or-else, set, keys, key-values, keys-like, delete, delete-if-exists, stdin
             \\Example: sqey mydb.db set key1 value1 key2 value2 && sqey mydb.db get key1 key2
         ;
         std.log.info(help, .{});
         return 0;
     }
 
-    const command_str = args.next() orelse {
+    var stdout_buffer: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    var state_machine: StateMachine = .{ .stdout = stdout };
+    defer state_machine.close();
+
+    const wrapper: ArgIteratorWrapper = .{
+        .iterator = &args,
+    };
+
+    return try processArgs(
+        wrapper, 
+        filepath, 
+        false, 
+        &state_machine,
+    );
+}
+
+pub fn processArgs(
+    args: anytype, 
+    filepath: [:0]const u8, 
+    is_stdin: bool,
+    state_machine: *StateMachine,
+) !u8 {
+    const command_str = try args.next() orelse {
         std.log.err(usage, .{});
         return 1;
     };
@@ -487,28 +564,28 @@ pub fn main() !u8 {
         break :result MessageType.Delete;
     } else if (std.mem.eql(u8, command_str, "delete-if-exists")) result: {
         break :result MessageType.DeleteIfExists;
+    } else if (std.mem.eql(u8, command_str, "stdin")) result: {
+        if (is_stdin) {
+            std.log.err("Cannot process \"stdin\" while already reading from stdin", .{});
+            return 1;
+        } else {
+            break :result MessageType.Stdin;
+        }
     } else {
-        std.log.err("Unknown command. Possible commands: get, get-or-else, set, keys, key-values, keys-like, delete, delete-if-exists", .{});
+        std.log.err("Unknown command. Possible commands: get, get-or-else, set, keys, key-values, keys-like, delete, delete-if-exists, stdin", .{});
         return 1;
     };
-
-    var state_machine: StateMachine = .{};
-    defer state_machine.close();
-
-    var stdout_buffer: [4096]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
 
     switch (command) {
         .Get => {
             var did_receive_valid_arg = false;
-            while (args.next()) |key| {
+            while (try args.next()) |key| {
                 if (!did_receive_valid_arg) {
                     did_receive_valid_arg = true;
-                    try state_machine.open(filepath, stdout);
+                    try state_machine.open(filepath);
                 }
 
-                try state_machine.process(.{ .Get = key });
+                try state_machine.process(@TypeOf(key), .{ .Get = key });
             }
             if (!did_receive_valid_arg) {
                 std.log.err("Missing at least one key for get command", .{});
@@ -517,18 +594,18 @@ pub fn main() !u8 {
         },
         .GetOrElse => {
             var did_receive_valid_arg = false;
-            while (args.next()) |key| {
-                const value = args.next() orelse {
+            while (try args.next()) |key| {
+                const value = try args.next() orelse {
                     std.log.err("Missing default value for key \"{s}\"", .{ key });
                     return 1;
                 };
 
                 if (!did_receive_valid_arg) {
                     did_receive_valid_arg = true;
-                    try state_machine.open(filepath, stdout);
+                    try state_machine.open(filepath);
                 }
 
-                try state_machine.process(.{ .GetOrElse = .{ .key = key, .value = value }});
+                try state_machine.process(@TypeOf(key), .{ .GetOrElse = .{ .key = key, .value = value }});
             }
             if (!did_receive_valid_arg) {
                 std.log.err("Missing at least one key value pair for get-or-else command", .{});
@@ -537,18 +614,18 @@ pub fn main() !u8 {
         },
         .Set => {
             var did_receive_valid_arg = false;
-            while (args.next()) |key| {
-                const value = args.next() orelse {
+            while (try args.next()) |key| {
+                const value = try args.next() orelse {
                     std.log.err("Missing value for key \"{s}\"", .{ key });
                     return 1;
                 };
 
                 if (!did_receive_valid_arg) {
                     did_receive_valid_arg = true;
-                    try state_machine.open(filepath, stdout);
+                    try state_machine.open(filepath);
                 }
 
-                try state_machine.process(.{ .Set = .{ .key = key, .value = value }});
+                try state_machine.process(@TypeOf(key), .{ .Set = .{ .key = key, .value = value }});
             }
             if (!did_receive_valid_arg) {
                 std.log.err("Missing at least one key value pair for set command", .{});
@@ -556,46 +633,46 @@ pub fn main() !u8 {
             }
         },
         .Keys => {
-            if (args.next()) |_| {
+            if (try args.next()) |_| {
                 std.log.err("Keys command doesn't accept extra arguments", .{});
                 return 1;
             }
 
-            try state_machine.open(filepath, stdout);
-            try state_machine.process(.{ .Keys = undefined });
+            try state_machine.open(filepath);
+            try state_machine.process([]const u8, .{ .Keys = undefined });
         },
         .KeyValues => {
-            if (args.next()) |_| {
+            if (try args.next()) |_| {
                 std.log.err("\"key-values\" command doesn't accept extra arguments", .{});
                 return 1;
             }
 
-            try state_machine.open(filepath, stdout);
-            try state_machine.process(.{ .KeyValues = undefined });
+            try state_machine.open(filepath);
+            try state_machine.process([]const u8, .{ .KeyValues = undefined });
         },
         .KeysLike => {
-            const pattern = args.next() orelse {
+            const pattern = try args.next() orelse {
                 std.log.err("Missing pattern for \"keys-like\"", .{});
                 return 1;
             };
 
-            if (args.next()) |_| {
+            if (try args.next()) |_| {
                 std.log.err("\"keys-like\" command doesn't accept any extra arguments after the pattern", .{});
                 return 1;
             }
 
-            try state_machine.open(filepath, stdout);
-            try state_machine.process(.{ .KeysLike = pattern });
+            try state_machine.open(filepath);
+            try state_machine.process(@TypeOf(pattern), .{ .KeysLike = pattern });
         },
         .Delete => {
             var did_receive_valid_arg = false;
-            while (args.next()) |key| {
+            while (try args.next()) |key| {
                 if (!did_receive_valid_arg) {
                     did_receive_valid_arg = true;
-                    try state_machine.open(filepath, stdout);
+                    try state_machine.open(filepath);
                 }
 
-                try state_machine.process(.{ .Delete = key });
+                try state_machine.process(@TypeOf(key), .{ .Delete = key });
             }
             if (!did_receive_valid_arg) {
                 std.log.err("Missing at least one key for delete command", .{});
@@ -604,19 +681,40 @@ pub fn main() !u8 {
         },
         .DeleteIfExists => {
             var did_receive_valid_arg = false;
-            while (args.next()) |key| {
+            while (try args.next()) |key| {
                 if (!did_receive_valid_arg) {
                     did_receive_valid_arg = true;
-                    try state_machine.open(filepath, stdout);
+                    try state_machine.open(filepath);
                 }
 
-                try state_machine.process(.{ .DeleteIfExists = key });
+                try state_machine.process(@TypeOf(key), .{ .DeleteIfExists = key });
             }
             if (!did_receive_valid_arg) {
                 std.log.err("Missing at least one key for \"delete-if-exists\" command", .{});
                 return 1;
             }
         },
+        .Stdin => {
+            if (try args.next()) |_| {
+                std.log.err("\"stdin\" command doesn't accept extra arguments", .{});
+                return 1;
+            }
+
+            if (!is_stdin) {
+                var stdin_buffer: [4096]u8 = undefined;
+                var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+                const stdin = &stdin_reader.interface;
+
+                var iterator: DelimiterIterator = .{
+                    .reader = stdin,
+                    .delimiter = '\n',
+                };
+                return try processArgs(&iterator, filepath, true, state_machine);
+            } else {
+                std.log.err("Processing stdin from stdin", .{});
+                return 1;
+            }
+        }
     }
 
     return 0;
